6bfc807cda2cbfbedbc75bb250375828bcb3c487
Quentin ADAM <waxzce@kan-op.com>
2010-07-27 16:33:53 +0200

    some change into the admin style

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,10 @@
 package controllers;
 
-import job.WritePages;
 import play.mvc.*;
 
 public class Application extends Controller {
 
     public static void index() {
         render();
-    }
-
-    public static void writePages() {
-        new WritePages().now();
-
-     //   redirect("/");
     }
 }
