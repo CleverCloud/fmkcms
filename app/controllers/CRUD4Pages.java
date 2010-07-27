a8560373d2f99014309375f9c44c305757d7db04
Quentin ADAM <waxzce@kan-op.com>
2010-07-27 18:06:06 +0200

    some change for crud class name

---
+++ app/controllers/CRUD4Pages.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controllers;
+
+import models.Page;
+
+/**
+ *
+ * @author waxzce
+ */
+@CRUD.For(Page.class)
+public class CRUD4Pages extends CRUD{
+
+}
---
+++ app/controllers/CRUD4Tags.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import models.Tag;
+
+/**
+ *
+ * @author waxzce
+ */
+@CRUD.For(Tag.class)
+public class CRUD4Tags extends CRUD {
+}
