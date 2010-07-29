c63eabb21b8ae181b63a219f3d6ffae02e1d3dff
Quentin ADAM <waxzce@kan-op.com>
2010-07-29 19:23:38 +0200

    Tag cache

--- app/models/Tag.java
+++ app/models/Tag.java
@@ -9,10 +9,12 @@ package models;
  * @author waxzce
  */
 import javax.persistence.*;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import play.db.jpa.*;
 
 @Entity
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE, include = "all")
 public class Tag extends Model implements Comparable<Tag> {
 
     public String name;
