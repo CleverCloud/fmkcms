9e27a1f2c08b5d7a4d76e7440fa4fdb457487ac7
Clement <cnivolle@gmail.com>
2010-08-08 00:05:49 +0200

    updating js editor

--- public/javascripts/get-textarea.js
+++ public/javascripts/get-textarea.js
@@ -1,9 +1,35 @@
-GENTICS.Aloha.settings = {
-			errorhandling : false,
-			ribbon: false,	
-			"i18n": {"current": "en"}
-		};
+/**
+ *
+ * @author cnivolle
+ */
 
-		jQuery(document).ready(function() {
-			jQuery('#object_content').aloha();
-		});
+//TODO: Making the HTML cleaner. <br> is diry.
+
+GENTICS.Aloha.settings = {
+    errorhandling : false,
+    ribbon: false,
+    "i18n": {
+        "current": "en"
+    }
+};
+
+jQuery(document).ready(function() {
+    // Here we hiding the texarea if JS is enabled
+    jQuery('#object_content').css('display','none');
+
+    //Putting a div where we can edit rich-text
+    jQuery("#object_content").after('<div id="editable2"></div>');
+
+    //Copying content of the textarea into the div
+    jQuery('#editable2').html(jQuery('#object_content').val());
+    //Making the div editable by aloha-editor
+    jQuery('#editable2').aloha();
+    /**
+     * When submitting, this function copy the content of 
+     * the div in the textarea.
+    */
+    jQuery("#page_form").submit(function(){
+    jQuery('#object_content').val(jQuery('#editable2').html());
+
+  });
+});
