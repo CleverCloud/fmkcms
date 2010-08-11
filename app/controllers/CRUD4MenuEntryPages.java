5e0d5bc6fb63cb87ead12ace4dc271bc8e27596a
Quentin ADAM <waxzce@kan-op.com>
2010-08-11 19:44:32 +0200

    menu begigning of form

---
+++ app/controllers/CRUD4MenuEntryPages.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import models.*;
+import play.mvc.With;
+
+/**
+ *
+ * @author waxzce
+ */
+@CRUD.For(MenuEntryPage.class)
+@With(CheckRights.class)
+public class CRUD4MenuEntryPages extends CRUD {
+
+    public static void quickAdd(String urlId) {
+        Page p = Page.getByUrlId(urlId);
+        if (p != null) {
+            MenuEntryPage mep = new MenuEntryPage();
+            mep.urlid = urlId;
+            mep.save();
+            renderJSON(mep);
+        }
+    }
+}
---
+++ public/javascripts/menu_management.js
@@ -0,0 +1,8 @@
+
+jQuery(document).ready(function() {
+    // Here we hiding the texarea if JS is enabled
+    jQuery('#add_page_btn').click(function(){
+        
+     })
+
+});
