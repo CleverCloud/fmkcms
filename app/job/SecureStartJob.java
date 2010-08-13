5197c60a2fa2487be1396ea70408829cbf7ec57d
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-08-13 15:35:34 +0200

    SecureStartJob cleanup

--- app/job/SecureStartJob.java
+++ app/job/SecureStartJob.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package job;
 
 import com.google.gson.Gson;
@@ -16,10 +12,8 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import play.Logger;
 import play.cache.Cache;
 import play.jobs.Every;
@@ -35,83 +29,58 @@ public class SecureStartJob extends Job<String> {
     private static Map<String, String> cacheMap = new HashMap<String, String>();
 
     private void fluchCache() {
-        Set<Map.Entry<String, String>> setcacheMap = SecureStartJob.cacheMap.entrySet();
-        Iterator<Map.Entry<String, String>> itc = setcacheMap.iterator();
-        while (itc.hasNext()) {
-            Map.Entry<String, String> entry = itc.next();
+        for (Map.Entry<String, String> entry : SecureStartJob.cacheMap.entrySet()) {
             Cache.delete("SECURE_" + entry.getKey());
-
         }
     }
 
     @Override
     public String doJobWithResult() throws Exception {
-        fluchCache();
+        this.fluchCache();
         // make a gson object
         Gson gson = new Gson();
         // find separator usefull in m$ window case
         String separator = System.getProperties().getProperty("file.separator");
         String pathSecurityLevels = Play.applicationPath + separator + "conf" + separator + "SecurityLevels.json";
         // find security levels
-        Type listOfSecurityLevelType = new TypeToken<List<SecurityLevel>>() {
-        }.getType();
+        Type listOfSecurityLevelType = new TypeToken<List<SecurityLevel>>() {}.getType();
         List<SecurityLevel> securityLevelList = gson.fromJson(new FileReader(pathSecurityLevels), listOfSecurityLevelType);
 
         // now find Roles
-        Type listOfRolesType = new TypeToken<List<Roles>>() {
-        }.getType();
+        Type listOfRolesType = new TypeToken<List<Roles>>() {}.getType();
         String pathRoles = Play.applicationPath + separator + "conf" + separator + "roles.json";
         List<Roles> listOfRoles = gson.fromJson(new FileReader(pathRoles), listOfRolesType);
         Map<String, String> rightsMap = new HashMap<String, String>();
         SecureStartJob.cacheMap = new HashMap<String, String>();
 
-
-        Iterator<Roles> itr = listOfRoles.iterator();
-        while (itr.hasNext()) {
-            Roles role = itr.next();
-            Iterator<String> it = role.rights_names.iterator();
-            while (it.hasNext()) {
-                String string = it.next();
+        for (Roles role : listOfRoles) {
+            for (String string : role.rights_names) {
                 rightsMap.put(string, role.name);
             }
         }
 
-        Iterator<SecurityLevel> itsl = securityLevelList.iterator();
-        while (itsl.hasNext()) {
-            SecurityLevel securityLevel = itsl.next();
-            Iterator<String> it = securityLevel.controllers.iterator();
-            while (it.hasNext()) {
-                String controllerName = it.next();
+        for (SecurityLevel securityLevel : securityLevelList) {
+            for (String controllerName : securityLevel.controllers) {
                 if (controllerName.contains("*")) {
                     try {
                         controllerName = controllerName.substring(0, controllerName.length() - 2);
                         Class c = Class.forName("controllers." + controllerName);
-                        Method[] cm = c.getMethods();
-                        List<Method> lm = Arrays.asList(cm);
-                        Iterator<Method> im = lm.iterator();
-                        while (im.hasNext()) {
-                            Method method = im.next();
+                        for (Method method : Arrays.asList(c.getMethods())) {
                             SecureStartJob.cacheMap.put(controllerName + "." + method.getName(), rightsMap.get(securityLevel.name));
                         }
-
                     } catch (ClassNotFoundException e) {
                         Logger.error("invalid rules : %s", controllerName);
                     }
-
-
                 } else {
                     SecureStartJob.cacheMap.put(controllerName, rightsMap.get(securityLevel.name));
                 }
             }
         }
 
-        Set<Map.Entry<String, String>> setcacheMap = SecureStartJob.cacheMap.entrySet();
-        Iterator<Map.Entry<String, String>> itc = setcacheMap.iterator();
-        while (itc.hasNext()) {
-            Map.Entry<String, String> entry = itc.next();
+        for (Map.Entry<String, String> entry : SecureStartJob.cacheMap.entrySet()) {
             Cache.add("SECURE_" + entry.getKey(), entry.getValue(), "100d");
         }
-
+        
         return SecureStartJob.cacheMap.toString();
     }
 }
