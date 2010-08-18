81ec7936d88bbee8b3395eb10e237edcedb55327
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-08-18 12:37:34 +0200

    blog: Initial comments support

--- app/models/blog/PostData.java
+++ app/models/blog/PostData.java
@@ -1,8 +1,12 @@
 package models.blog;
 
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import play.data.validation.Required;
 import play.db.jpa.Model;
 
@@ -24,10 +28,18 @@ public class PostData extends Model {
     @Required
     public User author;
 
+    @OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+    public List<Comment> comments;
+
     public PostData(User author, String title, String content) {
         this.author = author;
         this.title = title;
         this.content = content;
+    }
+
+    public PostData addComment(String author, String content) {
+        this.comments.add(new Comment(author, content));
+        return this.save();
     }
 
 }
