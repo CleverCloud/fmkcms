deaafbbee0f99ed495c0c3221203a4d94051fb41
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-08-18 16:46:55 +0200

    blog: return a List<Locale> now that we accept it in Post

--- app/controllers/BlogController.java
+++ app/controllers/BlogController.java
@@ -1,11 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
-import play.Logger;
 import play.cache.Cache;
 import play.libs.Images;
 import play.mvc.Controller;
@@ -17,15 +14,21 @@ import play.mvc.Http;
  */
 public class BlogController extends Controller {
 
-    public static Locale getBrowserLang(){
-        String lang = Http.Request.current().acceptLanguage().get(0);
-        Locale localeFromBrowser = null;
-        if (lang.split("-").length == 2) {
-            localeFromBrowser = new Locale(lang.split("-")[0],lang.split("-")[1]);
-        } else {
-            localeFromBrowser = new Locale(lang.split("-")[0]);
+    public static List<Locale> getBrowserLanguages(){
+        List<String> languages = Http.Request.current().acceptLanguage();
+        List<Locale> locales = new ArrayList<Locale>();
+
+        Locale locale = null;
+        for (String language : languages) {
+            if (language.split("-").length == 2)
+                locale = new Locale(language.split("-")[0], language.split("-")[1]);
+            else
+                locale = new Locale(language.split("-")[0]);
+
+            locales.add(locale);
         }
-        return localeFromBrowser;
+
+        return locales;
     }
 
     public static void captcha(String id) {
@@ -34,5 +37,6 @@ public class BlogController extends Controller {
         Cache.set(id, code, "10min");
         renderBinary(captcha);
     }
+    
 }
 
