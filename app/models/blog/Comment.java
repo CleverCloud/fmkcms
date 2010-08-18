9e096ba994bb5723e505624e59ff3638c7440b43
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-08-18 12:31:49 +0200

    blog: add Comment

---
+++ app/models/blog/Comment.java
@@ -0,0 +1,32 @@
+package models.blog;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+import play.data.validation.Required;
+import play.db.jpa.Model;
+
+/**
+ *
+ * @author keruspe
+ */
+@Entity
+public class Comment extends Model {
+
+    // TODO: Decide wether we support/require authentication here
+    @Required
+    public String author;
+
+    @Required
+    public Date postedAt;
+
+    @Lob
+    public String content;
+
+    public Comment(String author, String content) {
+        this.author = author;
+        this.content = content;
+        this.postedAt = new Date();
+    }
+
+}
