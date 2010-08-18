13a2855b43cd14d96c9351df933db3914a3c2c69
Clement <cnivolle@gmail.com>
2010-08-18 16:33:08 +0200

    Added a method to get the language to manage lang in the blog

--- app/controllers/BlogController.java
+++ app/controllers/BlogController.java
@@ -4,15 +4,29 @@
  */
 package controllers;
 
+import java.util.Locale;
+import play.Logger;
 import play.cache.Cache;
 import play.libs.Images;
 import play.mvc.Controller;
+import play.mvc.Http;
 
 /**
  *
  * @author clementnivolle
  */
 public class BlogController extends Controller {
+
+    public static Locale getBrowserLang(){
+        String lang = Http.Request.current().acceptLanguage().get(0);
+        Locale localeFromBrowser = null;
+        if (lang.split("-").length == 2) {
+            localeFromBrowser = new Locale(lang.split("-")[0],lang.split("-")[1]);
+        } else {
+            localeFromBrowser = new Locale(lang.split("-")[0]);
+        }
+        return localeFromBrowser;
+    }
 
     public static void captcha(String id) {
         Images.Captcha captcha = Images.captcha();
