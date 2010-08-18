ddf64a3772b25cbf5c2f00a2e3de49e77257ee4b
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-08-18 14:32:50 +0200

    blog: removeComment()

--- app/models/blog/PostData.java
+++ app/models/blog/PostData.java
@@ -1,6 +1,7 @@
 package models.blog;
 
 import java.util.List;
+import java.util.ListIterator;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -42,6 +43,31 @@ public class PostData extends Model {
         Comment comment = new Comment(author, content).save();
         this.comments.add(comment);
         return this.save();
+    }
+
+    public PostData removeComment(String author, String content, Boolean removeAll) {
+        // We want to got through it the reverse way to delete most recent comment first (duplicates)
+        // TODO: .size() or .size() - 1 for use with hasPrevious and deal withthe last element
+        ListIterator<Comment> iterator = this.comments.listIterator(this.comments.size());
+        Comment current = null;
+        while (iterator.hasPrevious()) {
+            current = iterator.previous();
+            // Continue if it's not the one we're looking for
+            if (! (current.author.equalsIgnoreCase(author) && current.content.equalsIgnoreCase(content)))
+                continue;
+
+            current.delete();
+
+            // Quit if we only want to remove one occurence of the comment
+            if (! removeAll)
+                break;
+        }
+
+        return this.save();
+    }
+
+    public PostData removeComment(String author, String content) {
+        return this.removeComment(author, content, false);
     }
 
 }
