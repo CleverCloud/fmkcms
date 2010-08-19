e584f69d34b72137e28af6fe2a1ba8db32f1685a
Clement <cnivolle@gmail.com>
2010-08-19 16:22:21 +0200

    Adding show & index methods into the controller.

--- app/controllers/BlogController.java
+++ app/controllers/BlogController.java
@@ -3,7 +3,9 @@ package controllers;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import models.blog.Post;
 import play.cache.Cache;
+import play.libs.Codec;
 import play.libs.Images;
 import play.mvc.Controller;
 import play.mvc.Http;
@@ -20,10 +22,11 @@ public class BlogController extends Controller {
 
         Locale locale = null;
         for (String language : languages) {
-            if (language.split("-").length == 2)
+            if (language.split("-").length == 2) {
                 locale = new Locale(language.split("-")[0], language.split("-")[1]);
-            else
+            } else {
                 locale = new Locale(language.split("-")[0]);
+            }
 
             locales.add(locale);
         }
@@ -32,11 +35,25 @@ public class BlogController extends Controller {
     }
 
     public static void captcha(String id) {
+
         Images.Captcha captcha = Images.captcha();
         String code = captcha.getText();
         Cache.set(id, code, "10min");
         renderBinary(captcha);
     }
-    
+
+    public static void show(Long id) {
+        Post post = Post.findById(id);
+        String randomID = Codec.UUID();
+        render(post, randomID);
+    }
+
+    public static void index() {
+
+        Post frontPost = Post.find("Order by postedAt desc").first();
+        List<Post> olderPosts = Post.find("Order by postedAt desc").from(1).fetch(10);
+
+        render(frontPost, olderPosts);
+    }
 }
 
