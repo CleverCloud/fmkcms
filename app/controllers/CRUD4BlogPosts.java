9a696041cca4320a95da0fb4b676727ba11b2051
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-08-20 15:11:10 +0200

    blog: Post* need authentication

--- app/controllers/CRUD4BlogPosts.java
+++ app/controllers/CRUD4BlogPosts.java
@@ -1,11 +1,13 @@
 package controllers;
 
 import models.blog.Post;
+import play.mvc.With;
 
 /**
  *
  * @author keruspe
  */
 @CRUD.For(Post.class)
+@With(CheckRights.class)
 public class CRUD4BlogPosts extends CRUD {
 }
