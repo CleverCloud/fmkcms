048f4db1fb6201b1a4bade271d7bcae92ce99009
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-08-25 17:21:29 +0200

    i18n: last fix, ready for merging !

--- app/models/Page.java
+++ app/models/Page.java
@@ -4,7 +4,6 @@ import controllers.UseCRUDFieldProvider;
 import crud.BooleanField;
 import java.util.List;
 import java.util.Locale;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
