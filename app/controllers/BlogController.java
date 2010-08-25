1f308fb88cf1e52677da1edbb201c3cfc0ede772
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-08-25 15:47:30 +0200

    blog: i18n: pass Post to views instead of PostRef

--- app/controllers/BlogController.java
+++ app/controllers/BlogController.java
@@ -1,7 +1,10 @@
 package controllers;
 
 import com.google.gson.Gson;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import models.blog.Post;
 import models.blog.PostRef;
 import play.cache.Cache;
 import play.data.validation.Validation;
@@ -25,34 +28,49 @@ public class BlogController extends Controller {
 
     public static void show(Long id) {
         PostRef postRef = PostRef.findById(id);
+        Post post = postRef.getPost(I18nController.getBrowserLanguages());
         String randomID = Codec.UUID();
-        render(postRef, randomID);
+        render(post, randomID);
     }
 
     public static void index() {
         PostRef frontPostRef = PostRef.find("Order by postedAt desc").first();
         List<PostRef> olderPostRefs = PostRef.find("Order by postedAt desc").from(1).fetch(10);
 
-        render(frontPostRef, olderPostRefs);
+        List<Locale> locales = I18nController.getBrowserLanguages();
+        Post frontPost = frontPostRef.getPost(locales);
+        List<Post> olderPosts = new ArrayList<Post>();
+        for(PostRef postRef : olderPostRefs) {
+            olderPosts.add(postRef.getPost(locales));
+        }
+
+        render(frontPost, olderPosts);
     }
 
-    public static void postComment(Long postRefId, String email, String pseudo, String password, String content, String code, String randomID) {
-        PostRef postRef = PostRef.findById(postRefId);
-        if (postRef == null)
+    public static void postComment(Long postId, String email, String pseudo, String password, String content, String code, String randomID) {
+        Post post = Post.findById(postId);
+        if (post == null)
             return;
 
         validation.equals(code, Cache.get(randomID)).message("Wrong validation code. Please reload a nother code.");
         if (Validation.hasErrors())
-            render("BlogController/show.html", postRef, randomID);
+            render("BlogController/show.html", post, randomID);
 
-        postRef.getPost(I18nController.getBrowserLanguages()).addComment(email, pseudo, password, content);
+        post.addComment(email, pseudo, password, content);
         Cache.delete(randomID);
-        BlogController.show(postRefId);
+        BlogController.show(post.postReference.id);
     }
 
     public static void listTagged(String tag) {
         List<PostRef> postRefs = PostRef.findTaggedWith(tag);
-        render(tag, postRefs);
+        List<Locale> locales = I18nController.getBrowserLanguages();
+        List<Post> posts = new ArrayList<Post>();
+
+        for (PostRef postRef : postRefs) {
+            posts.add(postRef.getPost(locales));
+        }
+
+        render(tag, posts);
     }
 
     public static void jsondump() {
