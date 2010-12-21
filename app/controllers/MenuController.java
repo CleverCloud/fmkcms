851592a919c8425a6c5414695ac1d5e36e5eae85
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-12-21 16:47:12 +0100

    menus are working (need submenus)

--- app/controllers/MenuController.java
+++ app/controllers/MenuController.java
@@ -3,6 +3,11 @@ package controllers;
 import java.util.ArrayList;
 import java.util.List;
 import models.menu.Menu;
+import models.menu.MenuItem;
+import models.menu.items.MenuItem_ControllerChain;
+import models.menu.items.MenuItem_LinkToPage;
+import models.menu.items.MenuItem_OutgoingURL;
+import models.menu.items.MenuItem_Title;
 import play.mvc.Controller;
 import play.mvc.With;
 
@@ -41,6 +46,20 @@ public class MenuController extends Controller {
 
    public static void doAddItem(String name) {
       Menu menu = Menu.findOrCreateByName(name);
+      MenuItem item;
+      String type = params.get("item.type");
+      if (type.equals("ControllerChain"))
+         item = new MenuItem_ControllerChain(params.get("item.value"));
+      else if (type.equals("LinkToPage"))
+         item = new MenuItem_LinkToPage(params.get("item.value"));
+      else if (type.equals("OutgoingURL"))
+         item = new MenuItem_OutgoingURL(params.get("item.value"));
+      else if (type.equals("Title"))
+         item = new MenuItem_Title(params.get("item.value"));
+      else
+         return;
+      item.save();
+      menu.addItem(item);
    }
 
 }
