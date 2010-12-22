66a7965beb281abd7abfe989cec3b262c3b43be6
Julien Durillon <julien.durillon@gmail.com>
2010-12-22 11:35:17 +0100

    Pour cni : faut que tu finisses la confirmation de suppression

--- app/controllers/PageController.java
+++ app/controllers/PageController.java
@@ -20,92 +20,107 @@ import play.mvc.With;
 @With(Secure.class)
 public class PageController extends Controller {
 
-    public static void listPages(Integer pagenumber) {
-        if (pagenumber == null)
-            pagenumber = 0;
-        List<PageRef> pages = PageRef.getPageRefPage(pagenumber, 20);
-        render(pages, pagenumber);
-    }
+   public static void listPages(Integer pagenumber) {
+      if (pagenumber == null) {
+         pagenumber = 0;
+      }
+      List<PageRef> pages = PageRef.getPageRefPage(pagenumber, 20);
+      render(pages, pagenumber);
+   }
 
-    public static void deletePage(String urlId, String language) {
-        Page page = Page.getPageByLocale(urlId, new Locale(language));
-        if (page == null)
-            return;
-        PageRef pageRef = page.pageReference;
-        page.delete();
-        if (Page.getFirstPageByPageRef(pageRef) == null)
-            pageRef.delete();
-        PageViewer.index();
-    }
+   public static void deletePage_confirm(String urlId, String language) {
+      Page page = Page.getPageByLocale(urlId, new Locale(language));
+      render(page);
+   }
 
-    public static void newPage(String action, String otherUrlId, String language) {
-        if (action.equals("delete"))
-            PageController.deletePage(otherUrlId, language);
-        Page otherPage = null;
+   public static void deletePage(String urlId, String language) {
+      Page page = Page.getPageByLocale(urlId, new Locale(language));
+      if (page == null) {
+         return;
+      }
+      PageRef pageRef = page.pageReference;
+      page.delete();
+      if (Page.getFirstPageByPageRef(pageRef) == null) {
+         pageRef.delete();
+      }
+      PageViewer.index();
+   }
 
-        if (otherUrlId != null) {
-            if (!otherUrlId.equals(""))
-                otherPage = models.Page.getPageByLocale(otherUrlId, new java.util.Locale(language));
-        }
+   public static void newPage(String action, String otherUrlId, String language) {
+      if (action.equals("delete")) {
+         PageController.deletePage(otherUrlId, language);
+      }
+      Page otherPage = null;
 
-        render(action, otherUrlId, language, otherPage);
-    }
+      if (otherUrlId != null) {
+         if (!otherUrlId.equals("")) {
+            otherPage = models.Page.getPageByLocale(otherUrlId, new java.util.Locale(language));
+         }
+      }
 
-    public static void doNewPage(String action, String otherUrlId, String otherLanguage) {
-        String urlId = otherUrlId;
-        Page page = null;
-        PageRef pageRef = null;
-        String tagsString = params.get("pageReference.tags");
+      render(action, otherUrlId, language, otherPage);
+   }
 
-        if (urlId != null && !urlId.equals(""))
-            page = Page.getPageByUrlId(urlId);
-        if (page != null)
-            pageRef = page.pageReference;
-        else
-            pageRef = PageController.doNewPageRef(action, otherUrlId, otherLanguage);
-        urlId = params.get("page.urlId");
+   public static void doNewPage(String action, String otherUrlId, String otherLanguage) {
+      String urlId = otherUrlId;
+      Page page = null;
+      PageRef pageRef = null;
+      String tagsString = params.get("pageReference.tags");
 
-        Set<Tag> tags = new TreeSet<Tag>();
-        if (tagsString != null && !tagsString.isEmpty()) {
-            for (String tag : Arrays.asList(tagsString.split(",")))
-                tags.add(Tag.findOrCreateByName(tag));
-        }
-        pageRef.tags = tags;
+      if (urlId != null && !urlId.equals("")) {
+         page = Page.getPageByUrlId(urlId);
+      }
+      if (page != null) {
+         pageRef = page.pageReference;
+      } else {
+         pageRef = PageController.doNewPageRef(action, otherUrlId, otherLanguage);
+      }
+      urlId = params.get("page.urlId");
 
-        if (!action.equals("edit"))
-            page = new Page();
-        page.pageReference = pageRef;
-        page.urlId = urlId;
-        page.title = params.get("page.title");
-        page.content = params.get("page.content");
-        page.language = params.get("page.language", Locale.class);
-        page.published = (params.get("page.published") == null) ? Boolean.FALSE : Boolean.TRUE;
+      Set<Tag> tags = new TreeSet<Tag>();
+      if (tagsString != null && !tagsString.isEmpty()) {
+         for (String tag : Arrays.asList(tagsString.split(","))) {
+            tags.add(Tag.findOrCreateByName(tag));
+         }
+      }
+      pageRef.tags = tags;
 
-        validation.valid(page);
-        if (Validation.hasErrors()) {
-            params.flash(); // add http parameters to the flash scope
-            Validation.keep(); // keep the errors for the next request
-            PageController.newPage(action, otherUrlId, otherLanguage);
-        }
-        page.pageReference.save();
-        page.save();
+      if (!action.equals("edit")) {
+         page = new Page();
+      }
+      page.pageReference = pageRef;
+      page.urlId = urlId;
+      page.title = params.get("page.title");
+      page.content = params.get("page.content");
+      page.language = params.get("page.language", Locale.class);
+      page.published = (params.get("page.published") == null) ? Boolean.FALSE : Boolean.TRUE;
 
-        if (page.published) 
-            PageViewer.page(urlId);
-        else
-            PageViewer.index();
-    }
+      validation.valid(page);
+      if (Validation.hasErrors()) {
+         params.flash(); // add http parameters to the flash scope
+         Validation.keep(); // keep the errors for the next request
+         PageController.newPage(action, otherUrlId, otherLanguage);
+      }
+      page.pageReference.save();
+      page.save();
 
-    private static PageRef doNewPageRef(String action, String otherUrlId, String otherLanguage) {
-        PageRef pageRef = new PageRef();
+      if (page.published) {
+         PageViewer.page(urlId);
+      } else {
+         PageViewer.index();
+      }
+   }
 
-        validation.valid(pageRef);
-        if (Validation.hasErrors()) {
-            params.flash(); // add http parameters to the flash scope
-            Validation.keep(); // keep the errors for the next request
-            PageController.newPage(action, otherUrlId, otherLanguage);
-        }
+   private static PageRef doNewPageRef(String action, String otherUrlId, String otherLanguage) {
+      PageRef pageRef = new PageRef();
 
-        return pageRef;
-    }
+      validation.valid(pageRef);
+      if (Validation.hasErrors()) {
+         params.flash(); // add http parameters to the flash scope
+         Validation.keep(); // keep the errors for the next request
+         PageController.newPage(action, otherUrlId, otherLanguage);
+      }
+
+      return pageRef;
+   }
 }
