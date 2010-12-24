0ee44e0376c0d6d10691a9c2e062a90612f87dba
Quentin ADAM <quentin.adam@waxzce.org>
2010-12-24 19:51:48 +0100

    continue clean the menu admin

--- app/controllers/MenuController.java
+++ app/controllers/MenuController.java
@@ -19,68 +19,73 @@ import play.mvc.With;
 public class MenuController extends Controller {
 
     public static void list() {
-      List<Menu> menus = Menu.findAll();
-      render(menus);
-   }
+        List<Menu> menus = Menu.findAll();
+        render(menus);
+    }
 
-   public static void edit(String action, String name) {
-      if (action.equals("delete")) {
-	 new RuntimeException("a delete menu is find - please replace code");
-      } else {
-         render(action, name);
-      }
-   }
+    public static void edit(String id) {
+        Menu menu = Menu.getByMongodStringId(id);
+        render(menu);
+    }
+    public static void edit_end(String id) {
+        Menu menu = Menu.getByMongodStringId(id);
+        menu.name = params.get("menu.name");
+        menu.save();
+        list();
+    }
 
-   public static void delete(String id){
-       Menu.getByMongodStringId(id).delete();
-       list();
-   }
+    public static void delete(String id) {
+        Menu.getByMongodStringId(id).delete();
+        list();
+    }
 
-   public static void newMenu() {
-      render();
-   }
+    public static void newMenu() {
+        render();
+    }
 
-   public static void doEdit(String action) {
-      Menu.findOrCreateByName(params.get("menu.name").replaceAll("[ #\\.]", "-"));
-      list();
-   }
+    public static void newMenu_end() {
+        Menu.findOrCreateByName(params.get("menu.name").replaceAll("[ #\\.]", "-"));
+        list();
+    }
 
-   public static void addItem(String name) {
-      if (name == null || name.isEmpty())
-         notFound();
-      List<String> types = new ArrayList<String>();
-      types.add("ControllerChain");
-      types.add("LinkToPage");
-      types.add("OutgoingURL");
-      types.add("Title");
-      render(name, types);
-   }
+    public static void addItem(String name) {
+        if (name == null || name.isEmpty()) {
+            notFound();
+        }
+        List<String> types = new ArrayList<String>();
+        types.add("ControllerChain");
+        types.add("LinkToPage");
+        types.add("OutgoingURL");
+        types.add("Title");
+        render(name, types);
+    }
 
-   public static void doAddItem(String name) {
-      Menu menu = Menu.findOrCreateByName(name);
-      MenuItem item;
-      String type = params.get("item.type");
-      String value = params.get("item.value");
-      String displayStr = params.get("item.display");
+    public static void doAddItem(String name) {
+        Menu menu = Menu.findOrCreateByName(name);
+        MenuItem item;
+        String type = params.get("item.type");
+        String value = params.get("item.value");
+        String displayStr = params.get("item.display");
 
-      if (displayStr == null || displayStr.isEmpty())
-         displayStr = value;
+        if (displayStr == null || displayStr.isEmpty()) {
+            displayStr = value;
+        }
 
-      if (type.equals("ControllerChain"))
-         item = new MenuItem_ControllerChain(value, displayStr);
-      else if (type.equals("LinkToPage"))
-         item = new MenuItem_LinkToPage(value, displayStr);
-      else if (type.equals("OutgoingURL"))
-         item = new MenuItem_OutgoingURL(value, displayStr);
-      else if (type.equals("Title"))
-         item = new MenuItem_Title(value, displayStr);
-      else
-         return;
+        if (type.equals("ControllerChain")) {
+            item = new MenuItem_ControllerChain(value, displayStr);
+        } else if (type.equals("LinkToPage")) {
+            item = new MenuItem_LinkToPage(value, displayStr);
+        } else if (type.equals("OutgoingURL")) {
+            item = new MenuItem_OutgoingURL(value, displayStr);
+        } else if (type.equals("Title")) {
+            item = new MenuItem_Title(value, displayStr);
+        } else {
+            return;
+        }
 
-      item.setMenu(Menu.findByName(params.get("item.subMenu")), menu);
-      item.cssLinkClass = params.get("item.cssLink");
-      item.save();
-      menu.addItem(item);
-   }
-
+        item.setMenu(Menu.findByName(params.get("item.subMenu")), menu);
+        item.cssLinkClass = params.get("item.cssLink");
+        item.save();
+        menu.addItem(item);
+    }
 }
