0b187641156578fa59667ea80a96ec4668c1d40d
Julien Durillon <julien.durillon@gmail.com>
2011-02-16 23:22:46 +0100

    Index support in entities + updated drivers.
    Added support for indexes: now unique=true generates an error if
    violated.

--- app/mongo/MongoEntity.java
+++ app/mongo/MongoEntity.java
@@ -4,6 +4,7 @@ import com.google.code.morphia.Datastore;
 import com.google.code.morphia.Morphia;
 import com.google.code.morphia.annotations.Id;
 import com.google.code.morphia.annotations.Transient;
+import com.mongodb.Mongo;
 import java.lang.reflect.Field;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,54 +18,63 @@ import play.Play;
 @SuppressWarnings("unchecked")
 public abstract class MongoEntity {
 
-    @Id
-    public ObjectId id;
+   @Id
+   public ObjectId id;
 
-    @Transient
-    private static Datastore datastore;
+   @Transient
+   private static Datastore datastore;
 
-    public static Datastore getDs() {
-        if (MongoEntity.datastore == null)
-            MongoEntity.datastore = (new Morphia()).createDatastore(Play.configuration.getProperty("fmkcms.db", "fmkcms"));
-        return MongoEntity.datastore;
-    }
+   public static Datastore getDs() {
+      if (MongoEntity.datastore == null) {
+         try {
+            Mongo mongo = new Mongo(Play.configuration.getProperty("fmkcms.db.host", "localhost"));
+            MongoEntity.datastore = (new Morphia()).createDatastore(mongo, Play.configuration.getProperty("fmkcms.db", "fmkcms"));
+         } catch (Exception e) {
+            play.Logger.error(e, "");
+         }
+      }
 
-    public <T extends MongoEntity> T save() {
-        MongoEntity.getDs().save(this);
-        return (T) MongoEntity.getDs().get(this);
-    }
+      return MongoEntity.datastore;
+   }
 
-    public <T extends MongoEntity> T create() {
-       MongoEntity.getDs().save(this);
-       return (T) MongoEntity.getDs().get(this.getClass(), this.id);
-    }
+   public <T extends MongoEntity> T save() {
+      play.Logger.info("save entity");
+      MongoEntity.getDs().save(this);
+      MongoEntity.getDs().ensureIndexes(this.getClass());
+      return (T) MongoEntity.getDs().get(this);
+   }
 
-    public <T extends MongoEntity> T refresh() {
-       if(this.id == null) {
-          return null;
-       } else {
-          T entity = (T) MongoEntity.getDs().get(this.getClass(), this.id);
-          for(Field field : this.getClass().getDeclaredFields()) {
-               try {
-                  if(field.get(this) != null) {
-                     field.set(entity, field.get(this));
-                  } else {
-                     field.set(this, field.get(entity));
-                  }
-               } catch (IllegalArgumentException ex) {
-                  Logger.getLogger(MongoEntity.class.getName()).log(Level.SEVERE, null, ex);
-               } catch (IllegalAccessException ex) {
-                  Logger.getLogger(MongoEntity.class.getName()).log(Level.SEVERE, null, ex);
+   public <T extends MongoEntity> T create() {
+      MongoEntity.getDs().save(this);
+      return (T) MongoEntity.getDs().get(this.getClass(), this.id);
+   }
+
+   public <T extends MongoEntity> T refresh() {
+      if (this.id == null) {
+         return null;
+      } else {
+         T entity = (T) MongoEntity.getDs().get(this.getClass(), this.id);
+         for (Field field : this.getClass().getDeclaredFields()) {
+            try {
+               if (field.get(this) != null) {
+                  field.set(entity, field.get(this));
+               } else {
+                  field.set(this, field.get(entity));
                }
-          }
+            } catch (IllegalArgumentException ex) {
+               Logger.getLogger(MongoEntity.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+               Logger.getLogger(MongoEntity.class.getName()).log(Level.SEVERE, null, ex);
+            }
+         }
 
-          return (T) this;
-       }
-    }
-    
-    public void delete() {
-        MongoEntity.getDs().delete(this);
-    }
+         return (T) this;
+      }
+   }
+
+   public void delete() {
+      MongoEntity.getDs().delete(this);
+   }
 
    @Override
    public boolean equals(Object obj) {
@@ -87,5 +97,4 @@ public abstract class MongoEntity {
       hash = 89 * hash + (this.id != null ? this.id.hashCode() : 0);
       return hash;
    }
-
 }
