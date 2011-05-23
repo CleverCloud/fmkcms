f6de274c755a8c69424674e21c05e47de34e3cbc
Quentin ADAM <quentin.adam@waxzce.org>
2011-05-23 23:36:51 +0200

    new methods for managers

--- app/models/i18n/TranslatableManager.java
+++ app/models/i18n/TranslatableManager.java
@@ -4,11 +4,16 @@
  */
 package models.i18n;
 
+import com.clevercloud.utils.locale.LocaleSet;
+import controllers.I18nController;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import mongo.MongoEntity;
@@ -28,13 +33,44 @@ public class TranslatableManager<T extends Translatable, R extends TranslatableR
       this.tr = tr;
    }
 
-  
-   
    public T getByMongodId(String id) {
       return (T) MongoEntity.getDs().find(t, "id", new ObjectId(id)).get();
    }
-   
+
    public R getRefByMongodId(String id) {
       return (R) MongoEntity.getDs().find(tr, "id", new ObjectId(id)).get();
+   }
+
+   public List<T> getPage(Integer pageNumber, Integer pageItemsNumber) {
+      if (pageNumber == null) {
+         pageNumber = 0;
+      }
+      return MongoEntity.getDs().find(t).offset(pageItemsNumber * pageNumber).limit(pageItemsNumber).asList();
+   }
+
+   public List<R> getRefPage(Integer pageNumber, Integer pageItemsNumber) {
+      if (pageNumber == null) {
+         pageNumber = 0;
+      }
+      return MongoEntity.getDs().find(tr).offset(pageItemsNumber * pageNumber).limit(pageItemsNumber).asList();
+   }
+
+   public Long number() {
+      return MongoEntity.getDs().getCount(t);
+   }
+
+   public Long refNumber() {
+      return MongoEntity.getDs().getCount(tr);
+   }
+
+   public T getBestForLocales(R ref, List<Locale> l) {
+      Map<Locale, T> m = ref.getAvailableLocalesMap();
+      LocaleSet ls = new LocaleSet();
+      ls.addAll(m.keySet());
+      return m.get(ls.getTheBestLocale(l));
+   }
+   
+   public T getBestForLocales(R ref) {
+      return getBestForLocales(ref, I18nController.getLanguages());
    }
 }
